5d620e11ecd0e3e183096707e33bf620eb6934ab
Sacha Refshauge <srefshauge@imagus.com.au>
2016-09-15 17:56:52 +1000

    S5P6818: Hardware DVP OV5640 to OpenCV Mat.

--- nanocams/nanocams.cpp
+++ nanocams/nanocams.cpp
@@ -36,125 +36,12 @@
 #include <linux/nxp_ion.h>
 
 #include <ion.h>
-#include <nx_fourcc.h>
-#include <libnxjpeghw.h>
 #include <nxp-v4l2.h>
 
-// --------------------------------------------------------
-static int debug = 1;
-
-#define dbg(level, fmt, arg...)		\
-	do {							\
-		if (debug >= (level))		\
-			fprintf(stderr, "D/%.3d: " fmt, __LINE__, ## arg);	\
-	} while (0)
+#include "opencv2/opencv.hpp"
 
 #define err(fmt, arg...)			\
 	fprintf(stderr, "E/%.3d: " fmt, __LINE__, ## arg)
-
-static struct timespec tpstart;
-
-static void time_start(void)
-{
-	clock_gettime(CLOCK_REALTIME, &tpstart);
-}
-
-static long time_us_delta(void)
-{
-	struct timespec tpend;
-	clock_gettime(CLOCK_REALTIME, &tpend);
-	long usec = ((tpend.tv_sec - tpstart.tv_sec) * 1000000 +
-				 (tpend.tv_nsec - tpstart.tv_nsec) / 1000);
-	tpstart = tpend;
-	return usec;
-}
-
-// --------------------------------------------------------
-
-/*
- * See system/core/init/util.c
- * Copyright (C) 2008 The Android Open Source Project
- */
-static char hardware[32];
-static unsigned revision = 0;
-
-static void get_hardware_name(char *hardware, unsigned int *revision)
-{
-	const char *cpuinfo = "/proc/cpuinfo";
-	char *data = NULL;
-	size_t len = 0, limit = 1024;
-	int fd, n;
-	char *x, *hw, *rev;
-
-	/* Hardware string was provided on kernel command line */
-	if (hardware[0])
-		return;
-
-	fd = open(cpuinfo, O_RDONLY);
-	if (fd < 0) return;
-
-	for (;;) {
-		x = (char *)realloc(data, limit);
-		if (!x) {
-			err("Failed to allocate memory to read %s\n", cpuinfo);
-			goto done;
-		}
-		data = x;
-
-		n = read(fd, data + len, limit - len);
-		if (n < 0) {
-			err("Failed reading %s: %s (%d)\n", cpuinfo, strerror(errno), errno);
-			goto done;
-		}
-		len += n;
-
-		if (len < limit)
-			break;
-
-		/* We filled the buffer, so increase size and loop to read more */
-		limit *= 2;
-	}
-
-	data[len] = 0;
-	hw = strstr(data, "\nHardware");
-	rev = strstr(data, "\nRevision");
-
-	if (hw) {
-		x = strstr(hw, ": ");
-		if (x) {
-			x += 2;
-			n = 0;
-			while (*x && *x != '\n') {
-				if (!isspace(*x))
-					hardware[n++] = tolower(*x);
-				x++;
-				if (n == 31) break;
-			}
-			hardware[n] = 0;
-		}
-	}
-
-	if (rev) {
-		x = strstr(rev, ": ");
-		if (x) {
-			*revision = strtoul(x + 2, 0, 16);
-		}
-	}
-
-done:
-	close(fd);
-	free(data);
-}
-
-static inline bool cpu_is_s5p4418(void)
-{
-	return !strcmp(hardware, "nanopi2");
-}
-
-static inline bool cpu_is_s5p6818(void)
-{
-	return !strcmp(hardware, "nanopi3");
-}
 
 // --------------------------------------------------------
 #ifndef ALIGN
@@ -208,11 +95,9 @@ int alloc_buffers(int ion_fd, int count, struct nxp_vid_buffer *bufs,
 	for (j = 0; j < plane_num; j++)
 		size[j] = get_size(format, j, width, height);
 
-	dbg(3, "Alloc buffer: count(%d), plane(%d), format(%x)\n", count, plane_num, format);
 	for (i = 0; i < count; i++) {
 		vb = &bufs[i];
 		vb->plane_num = plane_num;
-		dbg(3, "[Buffer %d] --->\n", i);
 		for (j = 0; j < plane_num; j++) {
 			ret = ion_alloc_fd(ion_fd, size[j], 0,
 					ION_HEAP_NXP_CONTIG_MASK, 0, &vb->fds[j]);
@@ -232,8 +117,6 @@ int alloc_buffers(int ion_fd, int count, struct nxp_vid_buffer *bufs,
 				return ret;
 			}
 			vb->sizes[j] = size[j];
-			dbg(3, "\tplane %d: fd(%d), size(%d), phys(0x%lx), virt(%p)\n",
-					j, vb->fds[j], vb->sizes[j], vb->phys[j], vb->virt[j]);
 		}
 	}
 
@@ -242,118 +125,35 @@ int alloc_buffers(int ion_fd, int count, struct nxp_vid_buffer *bufs,
 
 // --------------------------------------------------------
 #define MAX_BUFFER_COUNT	4
-#define FMT_SENDOR			V4L2_MBUS_FMT_YUYV8_2X8
+#define FMT_SENDOR		V4L2_MBUS_FMT_YUYV8_2X8
 
 #define __V4L2_S(cmd)		\
 	do {					\
 		int ret = cmd;		\
 		if (ret < 0) {		\
 			fprintf(stderr, "%.3d: `%s' = %d\n", __LINE__, #cmd, ret);	\
-			return ret;		\
+			/*return ret;*/		\
 		}					\
 	} while (0)
 
-static int init_preview(int width, int height, int format)
-{
-	__V4L2_S(v4l2_set_format(nxp_v4l2_decimator0, width, height, format));
-
-	__V4L2_S(v4l2_set_crop_with_pad(nxp_v4l2_decimator0, 2, 0, 0, width, height));
-	__V4L2_S(v4l2_set_crop(nxp_v4l2_decimator0, 0, 0, width, height));
-
-	// Set to default format (800x600) at first
-	if (width != 800)
-		__V4L2_S(v4l2_set_format(nxp_v4l2_sensor0, 800, 600, FMT_SENDOR));
-	__V4L2_S(v4l2_set_format(nxp_v4l2_sensor0, width, height, FMT_SENDOR));
-
-	__V4L2_S(v4l2_reqbuf(nxp_v4l2_decimator0, MAX_BUFFER_COUNT));
-	return 0;
-}
-
 /*
  * Note:
- *  - S5P4418: clipper0 + YUV422P, or decimator0 + YUV420M
- *  - S5P6818: clipper0 + YUV420M
+ *  - S5P6818: clipper0 + YUV422
  */
-static int init_capture(int width, int height)
+static int init_preview(int width, int height, int format)
 {
-	int format = V4L2_PIX_FMT_YUV422P;
-	if (cpu_is_s5p6818())
-		format = V4L2_PIX_FMT_YUV420M;
-
 	__V4L2_S(v4l2_set_format(nxp_v4l2_clipper0, width, height, format));
-	__V4L2_S(v4l2_set_crop(nxp_v4l2_clipper0, 0, 0, width, height));
+	__V4L2_S(v4l2_set_crop_with_pad(nxp_v4l2_clipper0, 2, 0, 0, width, height));
 
 	// Set to default format (800x600) at first
 	if (width != 800)
 		__V4L2_S(v4l2_set_format(nxp_v4l2_sensor0, 800, 600, FMT_SENDOR));
 	__V4L2_S(v4l2_set_format(nxp_v4l2_sensor0, width, height, FMT_SENDOR));
+
+	__V4L2_S(v4l2_set_ctrl(nxp_v4l2_sensor0, V4L2_CID_EXPOSURE_AUTO, 0));
 
 	__V4L2_S(v4l2_reqbuf(nxp_v4l2_clipper0, MAX_BUFFER_COUNT));
 	return 0;
-}
-
-static int init_display(int width, int height, int format)
-{
-	__V4L2_S(v4l2_set_format(nxp_v4l2_mlc0_video, width, height, format));
-
-	// Required for S5P6818
-	__V4L2_S(v4l2_set_crop_with_pad(nxp_v4l2_mlc0_video, 2, 0, 0, width, height));
-
-	// Adapt to LCD 1024x600
-	if (width == 1280 && height == 720) {
-		width  = 1024;
-		height =  720;
-	} else if (height > 1600) {
-		width  =  640;
-		height =  480;
-	} else if (height > 600) {
-		width  =  800;
-		height =  600;
-	}
-	dbg(3, "display crop [%d, %d]\n", width, height);
-	__V4L2_S(v4l2_set_crop(nxp_v4l2_mlc0_video, 0, 0, width, height));
-
-	__V4L2_S(v4l2_set_ctrl(nxp_v4l2_mlc0_video, V4L2_CID_MLC_VID_PRIORITY, 0));
-	__V4L2_S(v4l2_reqbuf(nxp_v4l2_mlc0_video, MAX_BUFFER_COUNT));
-	return 0;
-}
-
-static int save_handle_to_jpeg(struct nxp_vid_buffer *buf,
-		char *name, int width, int height)
-{
-	int ret = -1;
-	int align = cpu_is_s5p6818() ? 128 : 16;
-	int stride = ALIGN(width, align);
-
-	int fd = creat(name, 0644);
-	if (fd < 0) {
-		err("failed to create `%s', errno = %d\n", name, errno);
-		return ret;
-	}
-
-	int size = buf->sizes[0] + buf->sizes[1] + buf->sizes[2];
-	void *dst = malloc(size);
-	if (!dst) {
-		err("failed to malloc(%d), errno = %d\n", size, errno);
-		goto error;
-	}
-
-	ret = NX_JpegHWEncoding(dst, size, width, height, FOURCC_MVS0,
-			(unsigned int)buf->phys[0], 0, stride,
-			(unsigned int)buf->phys[1], 0, stride >> 1,
-			(unsigned int)buf->phys[2], 0, stride >> 1, true);
-	if (ret < 0) {
-		err("failed to encoding JPEG, ret = %d\n", ret);
-	} else {
-		ret = write(fd, dst, ret);
-		if (ret <= 0)
-			err("failed to write file, errno = %d\n", errno);
-	}
-
-	free(dst);
-error:
-	close(fd);
-	return ret;
 }
 
 static int do_preview(struct nxp_vid_buffer *bufs, int width, int height,
@@ -364,128 +164,68 @@ static int do_preview(struct nxp_vid_buffer *bufs, int width, int height,
 
 	for (i = 0; i < MAX_BUFFER_COUNT; i++) {
 		buf = &bufs[i];
-		dbg(3, "queue buffer %d\n", i);
-		v4l2_qbuf(nxp_v4l2_decimator0, buf->plane_num, i, buf, -1, NULL);
-	}
-
-	__V4L2_S(v4l2_streamon(nxp_v4l2_decimator0));
-
-	// fill all buffer for display
-	int index = 0;
-	for (i = 0; i < MAX_BUFFER_COUNT; i++) {
-		buf = &bufs[index];
-		dbg(2, "load buffer %d (%p)\n", index, buf);
-		v4l2_dqbuf(nxp_v4l2_decimator0, buf->plane_num, &index, NULL);
-		v4l2_qbuf(nxp_v4l2_decimator0, buf->plane_num, index, buf, -1, NULL);
-		index++;
-	}
-
-	int cap_index = 0;
-	int out_index = 0;
-	int out_dq_index = 0;
-	bool out_started = false;
-
-	dbg(1, "start preview %dx%d (%d frames)\n", width, height, preview_frames);
-	time_start();
-
-	for (i = 0; i < preview_frames; i++) {
-		buf = &bufs[cap_index];
-		v4l2_dqbuf(nxp_v4l2_decimator0, buf->plane_num, &cap_index, NULL);
-		v4l2_qbuf(nxp_v4l2_mlc0_video, buf->plane_num, out_index, buf, -1, NULL);
-
-		dbg(3, "[ %ld ] switch buffer %d\n", time_us_delta(), cap_index);
-		++out_index %= MAX_BUFFER_COUNT;
-
-		if (!out_started) {
-			if (v4l2_streamon(nxp_v4l2_mlc0_video) < 0)
-				goto __exit;
-			out_started = true;
-		}
-
-		v4l2_qbuf(nxp_v4l2_decimator0, buf->plane_num, cap_index, buf, -1, NULL);
-		if (i >= (MAX_BUFFER_COUNT - 1))
-			v4l2_dqbuf(nxp_v4l2_mlc0_video, buf->plane_num, &out_dq_index, NULL);
-	}
-
-	// stop preview
-	__V4L2_S(v4l2_streamoff(nxp_v4l2_mlc0_video));
-__exit:
-	__V4L2_S(v4l2_streamoff(nxp_v4l2_decimator0));
-	return 0;
-}
-
-int do_capture(struct nxp_vid_buffer *bufs, int width, int height,
-		int skip_frames, char *jpeg_file)
-{
-	struct nxp_vid_buffer *buf;
-	int i;
-
-	dbg(1, "capture image %dx%d --> %s\n", width, height, jpeg_file);
-
-	for (i = 0; i < MAX_BUFFER_COUNT; i++) {
-		buf = &bufs[i];
-		dbg(3, "queue buffer %d\n", i);
 		v4l2_qbuf(nxp_v4l2_clipper0, buf->plane_num, i, buf, -1, NULL);
 	}
 
 	__V4L2_S(v4l2_streamon(nxp_v4l2_clipper0));
 
+	// fill all buffer for display
 	int index = 0;
-	for (i = 0; i < skip_frames; i++) {
-		v4l2_dqbuf(nxp_v4l2_clipper0, buf->plane_num, &index, NULL);
+	for (i = 0; i < MAX_BUFFER_COUNT; i++) {
 		buf = &bufs[index];
+		v4l2_dqbuf(nxp_v4l2_clipper0, buf->plane_num, &index, NULL);
 		v4l2_qbuf(nxp_v4l2_clipper0, buf->plane_num, index, buf, -1, NULL);
-		dbg(2, "skip buffer %d (%p)\n", index, buf);
+		index++;
 	}
 
-	__V4L2_S(v4l2_streamoff(nxp_v4l2_clipper0));
+	int cap_index = 0;
+	int out_index = 0;
+	cv::namedWindow("preview", 1);
 
-	return save_handle_to_jpeg(buf, jpeg_file, width, height);
+	for (i = 0; i < preview_frames; i++) {
+		buf = &bufs[cap_index];
+		v4l2_dqbuf(nxp_v4l2_clipper0, buf->plane_num, &cap_index, NULL);
+
+		++out_index %= MAX_BUFFER_COUNT;
+		
+		cv::Mat mYUV(height, width, CV_8UC2);
+		cv::Mat mRGB(height, width, CV_8UC3);
+		unsigned char* p = mYUV.ptr<unsigned char>(0);
+		for (int i = 0, j = 0; i < 800 * 600 * 2; i+= 4, j++)
+		{
+			p[i] = buf->virt[1][j];
+			p[i+1] = buf->virt[0][j*2];
+			p[i+2] = buf->virt[2][j];
+			p[i+3] = buf->virt[0][j*2+1];
+		}
+		cv::cvtColor(mYUV, mRGB, CV_YUV2BGR_Y422, 3);
+		cv::imshow("preview", mRGB);
+		cv::waitKey(1);
+
+		v4l2_qbuf(nxp_v4l2_clipper0, buf->plane_num, cap_index, buf, -1, NULL);
+	}
+	
+	__V4L2_S(v4l2_streamoff(nxp_v4l2_clipper0));
+	return 0;
 }
 
 // --------------------------------------------------------
 
 /* MLC/video works improperly for YUV420M @800x600,1600x1200 */
-#define FMT_PREVIEW		V4L2_PIX_FMT_YUV422P
+#define FMT_PREVIEW	V4L2_PIX_FMT_YUV422P
 
 #ifndef ARRAY_SIZE
 #define ARRAY_SIZE(arr)	(int)(sizeof(arr) / sizeof((arr)[0]))
 #endif
 
-/* Supported frame size for sensor - OV5640 */
-static int sensor_frmsize_list[][2] = {
+/* Supported frame size for sensor - OV5640
 	{  640,  480, },
 	{  800,  600, },
 	{ 1280,  720, },
 	{ 1600, 1200, },
 	{ 2592, 1944, },
-};
+*/
 
-static void show_sensor_frmsize(void)
-{
-	fprintf(stderr, "Supported SIZE for preview/capture:\n");
-	for (int i = 0; i < ARRAY_SIZE(sensor_frmsize_list); i++) {
-		fprintf(stderr, "  %d - %dx%d\n", i,
-				sensor_frmsize_list[i][0], sensor_frmsize_list[i][1]);
-	}
-}
-
-static void get_preview_size(int n, int *width, int *height)
-{
-	if (n < 0 || n > 2) n = 0;
-
-	*width = sensor_frmsize_list[n][0];
-	*height = sensor_frmsize_list[n][1];
-}
-
-static void get_capture_size(int n, int *width, int *height)
-{
-	if (n < 0 || n >= ARRAY_SIZE(sensor_frmsize_list))
-		n = ARRAY_SIZE(sensor_frmsize_list) - 1;
-
-	*width = sensor_frmsize_list[n][0];
-	*height = sensor_frmsize_list[n][1];
-}
 
 // --------------------------------------------------------
 
@@ -493,31 +233,20 @@ static void show_usage(char *name)
 {
 	fprintf(stderr, "Usage: %s [OPTION]...\n\n", name);
 	fprintf(stderr, "Options:\n");
-	fprintf(stderr, "  -p SIZE     enable preview in size\n");
-	fprintf(stderr, "  -c SIZE     select capture size\n");
-	fprintf(stderr, "  -o FILE     save JPEG image to file\n");
 	fprintf(stderr, "  -n COUNT    show given number of frames then capture\n");
 	fprintf(stderr, "  -h          display this help and exit\n\n");
-	show_sensor_frmsize();
 	fprintf(stderr, "\n");
-	fprintf(stderr, "Camera demo for S5P4418/S5P6818 boards\n");
-	fprintf(stderr, "Copyright (C) FriendlyARM <http://www.friendlyarm.com>\n");
+	fprintf(stderr, "Camera demo for S5P6818 board\n");
 }
 
 int main(int argc, char *argv[])
 {
-	char *jpeg_file = NULL;
 	int width_p = 0, height_p = 0, count_p = 30;
-	int width_c = 0, height_c = 0;
 	int opt;
 
 	while (-1 != (opt = getopt(argc, argv, "p:c:n:o:d:h"))) {
 		switch (opt) {
-			case 'p': get_preview_size(atoi(optarg), &width_p, &height_p); break;
-			case 'c': get_capture_size(atoi(optarg), &width_c, &height_c); break;
-			case 'o': jpeg_file = optarg;     break;
 			case 'n': count_p = atoi(optarg); break;
-			case 'd': debug = atoi(optarg);   break;
 			case '?': fprintf(stderr, "\n");
 			case 'h': show_usage(argv[0]);  exit(0); break;
 			default:
@@ -525,19 +254,12 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	get_hardware_name(hardware, &revision);
-	dbg(2, "Board: %s, Rev %04x\n", hardware, revision);
-
-	if (jpeg_file && !width_c)
-		get_capture_size(-1, &width_c, &height_c);
-
-	if (width_p == 0 && (width_c == 0 || !jpeg_file)) {
-		dbg(1, "NO preview or capture request. Aborting...\n");
-		dbg(1, "try `%s -h' for help\n", argv[0]);
-		return 1;
-	}
+	// Hardcode what we want
+	width_p = 800;
+	height_p = 600;
 
 	int ion_fd = ion_open();
+	
 	if (ion_fd < 0) {
 		err("failed to ion_open, errno = %d\n", errno);
 		return -EINVAL;
@@ -545,11 +267,11 @@ int main(int argc, char *argv[])
 
 	struct V4l2UsageScheme s;
 	memset(&s, 0, sizeof(s));
-	s.useDecimator0 = true;
+	s.useDecimator0 = false;
 	s.useClipper0 = true;
 	s.useMlc0Video = true;
-
 	int ret = v4l2_init(&s);
+
 	if (ret < 0) {
 		err("initialize V4L2 failed, %d\n", ret);
 		close(ion_fd);
@@ -558,23 +280,13 @@ int main(int argc, char *argv[])
 
 	struct nxp_vid_buffer bufs[MAX_BUFFER_COUNT];
 	ret = alloc_buffers(ion_fd, MAX_BUFFER_COUNT, bufs,
-			(width_c > width_p) ? width_c : width_p,
-			(height_c > height_p) ? height_c : height_p, FMT_PREVIEW);
-	if (ret < 0)
-		goto exit;
+			width_p, height_p, FMT_PREVIEW);
 
-	if (width_p > 0) {
+	if (ret >= 0 && width_p > 0) {
 		init_preview(width_p, height_p, FMT_PREVIEW);
-		init_display(width_p, height_p, FMT_PREVIEW);
-		if ((ret = do_preview(bufs, width_p, height_p, count_p)))
-			goto exit;
+		do_preview(bufs, width_p, height_p, count_p);
 	}
 
-	if (jpeg_file && !init_capture(width_c, height_c)) {
-		ret = do_capture(bufs, width_c, height_c, 3, jpeg_file);
-	}
-
-exit:
 	v4l2_exit();
 	close(ion_fd);
 
